e128816dc774f53622bfe459855f38fc66957acf
Junior Clemente <juniorclemente.1508@gmail.com>
2025-05-13 10:35:35 -0500

    Solucion del ejercicio02

---
+++ script.js
@@ -0,0 +1,19 @@
+// Icon clicks
+document.querySelectorAll('.icon-bar i').forEach(icon => {
+    icon.addEventListener('click', () => {
+      alert(`Has hecho clic en: ${icon.title}`);
+    });
+  });
+  
+  // Clear button
+  document.getElementById('clearBtn').addEventListener('click', () => {
+    document.getElementById('volumeForm').reset();
+  });
+  
+  // Color controls
+  const rRange = document.getElementById('rRange');
+  const gRange = document.getElementById('gRange');
+  const bRange = document.getElementById('bRange');
+  
+  const rValue = document.getElementById('rValue');
+  const gValue = document.getElementBy
